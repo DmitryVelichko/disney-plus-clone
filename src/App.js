7979d54c1b96c10d9a8686f9e8dc60ba1e7ffc6a
Dmitry Velichko <soleil-2005@mail.ru>
2022-04-06 21:10:16 +0300

    New commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { Router } from 'react-router'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h3>Hello React</h3>
+      <Router />
     </div>
   );
 }
