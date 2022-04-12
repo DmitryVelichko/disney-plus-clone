116c289e0980b7fe643dd0bb4930db65e45a04f9
Dmitry Velichko <soleil-2005@mail.ru>
2022-04-12 10:20:25 +0300

    New commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -60,7 +60,8 @@ const Login = (props) => {
     margin-right: auto;
     margin-left: auto;
     transition-timing-function: ease-out;
-    
+    transition: opacity 0.2s;
+    width: 100%;
 
   `;
 
