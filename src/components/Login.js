32a68822c6e429f3689cdad1fd3f8e469efe4f9b
Dmitry Velichko <soleil-2005@mail.ru>
2022-04-12 10:29:50 +0300

    New commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,72 +5,76 @@ const Login = (props) => {
     <Container>
       <Content>
         <CTA>
-          <CTALogoOne src='/images/cta-logo-one.svg' alt='logos'/>
+          <CTALogoOne src='/images/cta-logo-one.svg' alt='logos' />
+          <SignUp>GET ALL THERE</SignUp>
         </CTA>
         <BgImage />
       </Content>
     </Container>
   );
 };
-  const Container = styled.section`
-    overflow: hidden;
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-    height: 100vh;
-  `;
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
 
-  const Content = styled.div`
-    margin-bottom: 10vw;
-    width: 100%;
-    height: 100%;
-    position: relative;
-    min-height: 100vh;
-    box-sizing: border-box;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    padding: 80px 40px;
-  `;
+const Content = styled.div`
+  margin-bottom: 10vw;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+`;
 
-  const BgImage = styled.div `
-    background-image: url('/images/login-background.jpg');
-    height: 100%;
-    background-position: top;
-    background-size: cover;
-    background-repeat: no-repeat;
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    z-index: -1;
-  `
+const BgImage = styled.div`
+  background-image: url('/images/login-background.jpg');
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
 
-  const CTA = styled.div`
-    margin-bottom: 2vw;
-    max-width: 650px;
-    flex-wrap: wrap;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-top: 0;
-    text-align: center;
-    margin-right: auto;
-    margin-left: auto;
-    transition-timing-function: ease-out;
-    transition: opacity 0.2s;
-    width: 100%;
+const CTA = styled.div`
+  margin-bottom: 2vw;
+  max-width: 650px;
+  flex-wrap: wrap;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 0;
+  text-align: center;
+  margin-right: auto;
+  margin-left: auto;
+  transition-timing-function: ease-out;
+  transition: opacity 0.2s;
+  width: 100%;
+`;
 
-  `;
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
 
-  const CTALogoOne = styled.img`
-    margin-bottom: 12px;
-    max-width: 600px;
-    min-height: 1px;
-    display: block;
-    width: 100%;
-  `
+const SignUp = styled.a`
+  font-weight: bold;
+`;
 
 export default Login;
