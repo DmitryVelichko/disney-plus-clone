043250c3b2fd67228f76534d5664e1fbdf075750
Dmitry Velichko <soleil-2005@mail.ru>
2022-04-16 23:55:59 +0300

    New commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -87,6 +87,7 @@ const NavMenu = styled.div`
       right: 0px;
       transform-origin: left center;
       transform: scaleX(0);
+      transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
     }
   }
 
