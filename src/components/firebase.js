5e173f65e53ca6c45809a7f9d4c6646f70592f56
Dmitry Velichko <soleil-2005@mail.ru>
2022-04-18 21:11:26 +0300

    New commit

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -13,3 +13,7 @@ const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 const storage = firebase.storage();
+
+export { auth, provider, storage };
+
+export default db;
