2090976b002656b6a0f997ff1336a0bef59e78ea
Dmitry Velichko <soleil-2005@mail.ru>
2022-04-19 09:57:35 +0300

    New commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/storage';
+
 const firebaseConfig = {
   apiKey: "AIzaSyBsqCN-CAzb-3K_MLHMMmVXiOrFwqYHhuM",
   authDomain: "disneyplus-clone-94dc4.firebaseapp.com",
@@ -12,8 +17,8 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
-const storage = firebase.storage();
+// const storage = firebase.storage();
 
-export { auth, provider, storage };
+export { auth, provider };
 
 export default db;
