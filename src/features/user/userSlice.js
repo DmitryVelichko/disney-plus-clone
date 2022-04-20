c0a8e332cd54d2ed7c1acc94289492d7d4c0f371
Dmitry Velichko <soleil-2005@mail.ru>
2022-04-20 20:56:32 +0300

    New commit

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,25 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    name: '',
-    email: '',
-    photo: '',
+  name: '',
+  email: '',
+  photo: '',
 };
 
 const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        setUserLoginDetails: (state, action) => {
-            state.name = action.payload.name;
-            state.email = action.payload.email;
-            state.photo = action.payload.photo;
-        },
+  name: 'user',
+  initialState,
+  reducers: {
+    setUserLoginDetails: (state, action) => {
+      state.name = action.payload.name;
+      state.email = action.payload.email;
+      state.photo = action.payload.photo;
+    },
 
-        setSignOutState: (state) => {
-            state.name = null;
-            state.email = null;
-            state.photo = null;
-        }
-    }
-})
+    setSignOutState: (state) => {
+      state.name = null;
+      state.email = null;
+      state.photo = null;
+    },
+  },
+});
+
+export const { setUserLoginDetails, setSignOutState } = userSlice.actions;
