ec449a40476d5d9822fca461c04d6fe29ac6253c
Dmitry Velichko <soleil-2005@mail.ru>
2022-04-20 21:09:06 +0300

    New commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { userReducer } from '../features/user/userSlice';
 
-export default configureStore ({
-    reducer: {},
-    middleware: getDefaultMiddleware({
-        serializableCheck: false,
-    })
-})
+export default configureStore({
+  reducer: {
+    user: userReducer,
+  },
+  middleware: getDefaultMiddleware({
+    serializableCheck: false,
+  }),
+});
