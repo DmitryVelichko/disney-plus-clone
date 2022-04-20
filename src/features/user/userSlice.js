89bdf749f3b8850ac2310ea618ba4daa23d02644
Dmitry Velichko <soleil-2005@mail.ru>
2022-04-20 20:59:59 +0300

    New commit

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -28,3 +28,4 @@ export const { setUserLoginDetails, setSignOutState } = userSlice.actions;
 
 export const selectUserName = state => state.user.name;
 export const selectUserEmail = state => state.user.email;
+export const selectUserPhoto = state => state.user.photo;
